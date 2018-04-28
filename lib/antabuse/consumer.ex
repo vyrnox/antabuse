672652ce2a92f1bebbbab44b57ee68ece8e1e7fa
Vyrnox <vyrnox@gmail.com>
2018-04-28 04:18:55 -0400

    Broken work in progress.

--- lib/antabuse/consumer.ex
+++ lib/antabuse/consumer.ex
@@ -7,24 +7,51 @@ defmodule Antabuse.Consumer do
     Consumer.start_link(__MODULE__)
   end
 
+  @prefix "!"
+
+  defp valid_command?(msg) do
+    String.starts_with?(msg.content, @prefix)
+  end
+
+  defp execute(["!mute", username], msg, guild) do
+    IO.puts "Got mute command: #{username}"
+    {:ok, member} = Api.get_guild_member(guild.id, username)
+    IO.puts "Got member: #{member.username} with role ID list: #{member.roles}"
+    # Save user's role state.
+    # Remove all user's roles.
+    # Add mute role to user.
+    # Deafen and mute the user.
+    Api.create_message(msg.channel_id, content: "Muting, deafening and role smashing: #{username}")
+  end
+
+  defp execute(["!unmute", username], msg, guild) do
+    IO.puts "Got unmute command: #{username}"
+    # Restore user's role state and clear dictionary entry for user.
+    # Remove mute role from user.
+    # Undeafen and unmute the user.
+    Api.create_message(msg.channel_id, content: "Unmuting, undeafening and role restoring: #{username}")
+  end
+
+  def handle_event({:READY, _, _w_state}) do
+    now = DateTime.utc_now
+    Api.update_status(:online, "you get rekt", 3)
+    IO.puts("Antabuse ready at #{now.month}/#{now.day}/#{now.year} #{now.hour}:#{now.minute}:#{now.second}")
+  end
+
   def handle_event({:MESSAGE_CREATE, {msg}, _ws_state}) do
-    case msg.content do
-      "!mute" ->
-        Api.create_message(msg.channel_id, "Stub for !mute")
-        # Save user's role state.
-        # Remove all user's roles.
-        # Add mute role to user.
-        # Deafen and mute the user.
+    {:ok, channel} = Api.get_channel(msg.channel_id)
+    {:ok, guild} = Api.get_guild(channel.guild_id)
+    
+    IO.puts "(#{guild.name}::#{channel.name}) #{msg.author.username}: #{msg.content}"
 
-      "!unmute" ->
-        Api.create_message(msg.channel_id, "Stub for !unmute")
-        # Restore user's role state and clear dictionary entry for user.
-        # Remove mute role from user.
-        # Undeafen and unmute the user.
-
-      _ ->
-        :ignore
+    if valid_command?(msg) do
+      msg.content
+      |> String.trim()
+      |> String.split(" ")
+      |> execute(msg, guild)
+      IO.puts "Got: #{msg.content}"
     end
+
   end
 
   # Default event handler, if you don't include this, your consumer WILL crash if
